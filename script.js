f3151f212eb4fdbd53d0e53b6a8a6cc1712cc5fe
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2023-03-10 22:36:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const submitButton = document.getElementById('submit');
 const message = document.getElementById('message');
+const hero = document.getElementById('hero');
+
 
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // prevent the form from submitting
@@ -13,8 +15,10 @@ form.addEventListener('submit', function(event) {
   if (email.indexOf('@') === -1 || password.length < 8) {
     message.textContent = '* Invalid email or password!';
     message.style.color = 'red';
+    hero.style.boxShadow = "0px 0px 10px red";
   } else {
     message.textContent = 'Valid email and password!';
     message.style.color = 'green';
+    hero.style.boxShadow = "0px 0px 10px green";
   }
 });
